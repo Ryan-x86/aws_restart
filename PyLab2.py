fe8ed5dac46121ddd9021b01c475e306ec09bfbc
Ryan-x86 <72678775+Ryan-x86@users.noreply.github.com>
2023-08-23 11:19:56 +0800

    Add files via upload

---
+++ PyLab2.py
@@ -0,0 +1,38 @@
+#print("Python has three numeric types: int, float, and complex")
+
+#String Data Type
+"""Creating a Variable
+myValue = 1
+print(myValue)
+print(type(myValue))
+
+print(str(myValue) + " is the data type " + str(type(myValue)))
+"""
+
+#Float Data Type
+"""myValue = 3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+"""
+
+#Complex Data Type
+"""myValue = 5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+"""
+
+#Bool Data type
+"""myValue = True
+print(myValue)
+print(type(myVale))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+"""
+
+#Bool Data Type (False)
+"""myValue = True
+print(myValue)
+print(type(myVale))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+"""
---
+++ PyLab3.py
@@ -0,0 +1,27 @@
+#String Exercise
+
+"""myString = "This is a string."
+print(type(myString))
+
+print(myString + " is of the data type " + str(type(myString)))
+"""
+
+#String Concantenation
+
+"""firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+"""
+
+#Input Strings
+
+name = input("What is your name: ")
+print(name)
+
+#Output Strings
+
+color = input("What is your favorite color?: ")
+animal = input("What is your favorite animal?: ")
+
+print("{}, you like a {} {}!".format(name,color,animal))
